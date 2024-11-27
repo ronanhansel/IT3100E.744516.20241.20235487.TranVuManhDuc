8a6dde7cfeaa6f0d70600b51004801472bd7c7d4
Ronan Hansel <ronanhansel@gmail.com>
2024-11-27 23:14:43 +0700

    feat: ex5

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -1,9 +1,40 @@
 public class DigitalVideoDisc {
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
+    private static int nbDigitalVideoDiscs = 0;
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private int id;
+ 	
+	public int getId() {
+        return id;
+    }
+
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        this.id = ++nbDigitalVideoDiscs;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this(title);
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this(title, category, cost);
+        this.director = director;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this(title, category, director, cost);
+        this.length = length;
+    }
 	public String getTitle() {
 		return title;
 	}

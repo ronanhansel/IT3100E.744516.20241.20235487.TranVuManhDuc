c5b8fc407f6c903faf141f492fd8ea2582643439
Ronan Hansel <ronanhansel@gmail.com>
2024-11-28 13:43:29 +0700

    feat: lab04

---
+++ hust/soict/dsai/aims/media/CompactDisc.java
@@ -0,0 +1,49 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc implements Playable {
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<>();
+
+    public CompactDisc(int id, String title, String category, float cost, int length, String director, String artist) {
+        super(id, title, category, cost, length, director);
+        this.artist = artist;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void addTrack(Track track) {
+        if (!tracks.contains(track)) {
+            tracks.add(track);
+        } else {
+            System.out.println("Track already exists in the list.");
+        }
+    }
+
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+        } else {
+            System.out.println("Track does not exist in the list.");
+        }
+    }
+
+    public int getLength() {
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing CD: " + this.getTitle() + " by " + this.getArtist());
+        for (Track track : tracks) {
+            track.play();
+        }
+    }
+}
